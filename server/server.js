f13942c9d7748df614266cfc680552620cc4bac2
IcusFMagni <danika.ragnhild@gmail.com>
2017-12-12 10:22:03 -0600

    Better Mongo Connection

--- server/server.js
+++ server/server.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
-var mongooseConnection = require('./modules/mongoose-connection');
+require('./modules/mongoose-connection');
 
 var rentals = require('./routes/rentals');
 var listings = require('./routes/listings');
@@ -11,7 +11,7 @@ var port = process.env.PORT || 5000;
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(express.static('server/public'));
-mongooseConnection.connect();
+
 
 app.use('/rentals', rentals);
 app.use('/listings', listings);
